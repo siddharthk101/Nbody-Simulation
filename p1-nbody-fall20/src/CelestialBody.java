04ab8c376cd5f995974c5a0179efb5aba91c5b15
siddharthk101 <97913183+siddharthk101@users.noreply.github.com>
2022-01-17 13:35:19 -0600

    Add files via upload

---
+++ p1-nbody-fall20/src/CelestialBody.java
@@ -0,0 +1,135 @@
+
+
+/**
+ * Celestial Body class for NBody
+ * @author ola
+ *
+ */
+public class CelestialBody {
+
+	private double myXPos;
+	private double myYPos;
+	private double myXVel;
+	private double myYVel;
+	private double myMass;
+	private String myFileName;
+
+	/**
+	 * Create a Body from parameters	
+	 * @param xp initial x position
+	 * @param yp initial y position
+	 * @param xv initial x velocity
+	 * @param yv initial y velocity
+	 * @param mass of object
+	 * @param filename of image for object animation
+	 */
+	public CelestialBody(double xp, double yp, double xv,
+			             double yv, double mass, String filename){
+		myXPos = xp;
+		myYPos = yp;
+		myXVel = xv;
+		myYVel = yv;
+		myMass = mass;
+		myFileName = filename;
+
+	}
+
+	/**
+	 * Copy constructor: copy instance variables from one
+	 * body to this body
+	 * @param b used to initialize this body
+	 */
+	public CelestialBody(CelestialBody b){
+		myXPos = b.getX();
+		myYPos = b.getY();
+		myXVel = b.getXVel();
+		myYVel = b.getYVel();
+		myMass = b.getMass();
+		myFileName = b.getName();
+	}
+
+	public double getX() {
+		return myXPos;
+	}
+	public double getY() {
+		return myYPos;
+	}
+	public double getXVel() {
+		return myXVel;
+	}
+	/**
+	 * Return y-velocity of this Body.
+	 * @return value of y-velocity.
+	 */
+	public double getYVel() {
+		return myYVel;
+	}
+	
+	public double getMass() {
+		return myMass;
+	}
+	public String getName() {
+		return myFileName;
+	}
+
+	/**
+	 * Return the distance between this body and another
+	 * @param b the other body to which distance is calculated
+	 * @return distance between this body and b
+	 */
+	public double calcDistance(CelestialBody b) {
+		return Math.sqrt((myXPos-b.getX())*(myXPos-b.getX())+(myYPos-b.getY())*(myYPos-b.getY()));
+	}
+
+	public double calcForceExertedBy(CelestialBody b) {
+		return (6.67*1e-11)*myMass*b.getMass()/(this.calcDistance(b)*this.calcDistance(b));
+	}
+
+	public double calcForceExertedByX(CelestialBody b) {
+		return this.calcForceExertedBy(b)*(b.getX()-myXPos)/this.calcDistance(b);
+	}
+	public double calcForceExertedByY(CelestialBody b) {
+		return this.calcForceExertedBy(b)*(b.getY()-myYPos)/this.calcDistance(b);
+	}
+
+	public double calcNetForceExertedByX(CelestialBody[] bodies) {
+		double sum = 0.0;
+		for(CelestialBody b: bodies){
+			if (! b.equals(this)){
+				sum += this.calcForceExertedByX(b);
+			}
+		}
+		return sum;
+	}
+
+	public double calcNetForceExertedByY(CelestialBody[] bodies) {
+		double sum = 0.0;
+		for(CelestialBody b: bodies){
+			if (! b.equals(this)){
+				sum += this.calcForceExertedByY(b);
+			}
+		}
+		return sum;
+	}
+
+	public void update(double deltaT, 
+			           double xforce, double yforce) {
+		double ax = xforce/myMass;
+		double ay = yforce/myMass;
+		double nvx = myXVel + deltaT*ax;
+		double nvy = myYVel + deltaT*ay;
+		double nx = myXPos + deltaT*nvx;
+		double ny = myYPos + deltaT*nvy;
+		myXPos = nx;
+		myYPos = ny;
+		myXVel = nvx;
+		myYVel = nvy;
+	}
+
+	/**
+	 * Draws this planet's image at its current position
+	 */
+	public void draw() {
+		StdDraw.picture(myXPos,myYPos,"images/"+myFileName);
+	}
+}
